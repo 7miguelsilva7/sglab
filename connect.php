48a856a2d7ecf9f79e316344fbf971085dbc8635
marcoschags <marcoschags@hostmail.com>
2016-11-07 13:22:17 -0300

    variáveis no arquivo connect

--- connect.php
+++ connect.php
@@ -1,14 +1,11 @@
 <?php
-$server="localhost";
+$server="127.0.0.1";
 $db="sglab";
 $user="root";
-$pass="root";
+$pass="";
 $version="0.9d";
 $mysqlport=3306;
 $pchartfolder="class/pchart2";
 
 
-$conn=new PDO("mysql:host=localhost;dbname=sglab", 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
-
-
+$conn=new PDO("mysql:host=$server;dbname=$db", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
