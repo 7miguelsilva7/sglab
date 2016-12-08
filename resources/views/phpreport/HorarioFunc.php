acbdf7b037c3ef555c4ec586a5f91cc2a2a3f651
Miguel Silva <7miguelsilva7@gmail.com>
2016-12-08 10:52:39 -0300

    edição

--- resources/views/phpreport/HorarioFunc.php
+++ resources/views/phpreport/HorarioFunc.php
@@ -28,7 +28,7 @@ $PHPJasperXML->arrayParameter=  [
 
 
 
-$PHPJasperXML->load_xml_file("/home/aetji649/sglab/jrxmlreport/PerfilLaboratorios.jrxml");
+$PHPJasperXML->load_xml_file("/home/aetji649/sglab/jrxmlreport/HorarioFunc.jrxml");
 
 $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db);
 $PHPJasperXML->outpage("D");    //page output method I:standard output  D:Download file
