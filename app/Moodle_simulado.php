3025b047da1a55b3af6bdb9c8741fac9f1095998
7miguelsilva7 <7miguelsilva7@gmail.com>
2016-12-20 18:05:26 -0300

    retirado campo horario turma

--- app/Moodle_simulado.php
+++ app/Moodle_simulado.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * Class Moodle_simulado.
  *
- * @author  The scaffold-interface created at 2016-12-15 06:44:06pm
+ * @author  The scaffold-interface created at 2016-12-20 04:11:13pm
  * @link  https://github.com/amranidev/scaffold-interface
  */
 class Moodle_simulado extends Model
